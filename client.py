316b3b24f27956fbe00aed02ec35f4c9bd5f7093
fumcheg <e7013ustas@gmail.com>
2023-07-24 17:16:16 +0300

    disconnect callback

--- client.py
+++ client.py
@@ -18,20 +18,25 @@ def client_init(client_id):
     client.on_publish = on_publish
     client.on_subscribe = on_subscribe
     client.on_message = on_message
+    client.on_disconnect = on_disconnect
     return client
 
-def on_connect(client_id, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     print(f"client <{userdata['id']}>: Connection result is {('OK', 'NOT OK')[bool(rc)]}")
 
-def on_publish(client_id, userdata, mid):
+def on_publish(client, userdata, mid):
     print(f"client <{userdata['id']}>: Publishing message id={mid} -> Message payload:\n{u_data['pub_msg']}")
 
-def on_message(client_id, userdata, message):
+def on_message(client, userdata, message):
     print(f"client <{userdata['id']}>: Recieved message with topic={message.topic}, " +
             f"payload={message.payload}, qos={message.qos}, retain={message.retain}")
 
-def on_subscribe(client_id, userdata, mid, granted_qos):
+def on_subscribe(client, userdata, mid, granted_qos):
     print(f"client <{userdata['id']}>: Subscribing request id={mid}, qos={', '.join(str(i) for i in granted_qos)}")
+
+def on_disconnect(client, userdata, rc):
+    print(f'MQTT client is disconnected')
+
 
 def pub_JSON(client):
     pub_msg = read_JSON(JSON_FILE)
@@ -52,6 +57,7 @@ if __name__ == "__main__":
     except TimeoutError:
         print("\nERR: Connection timeout")
     except KeyboardInterrupt:
+        client.disconnect()
         print("\nERR: Script was terminated by user")
     except OSError:
         print("\nERR: Network problem, probably")
